343a73d9b3dd9c84e26d613c3cbc8071f2bc37a6
Jarette Greene <jarettegreene09@gmail.com>
2022-11-21 09:30:56 -0600

    helper.hpp comments

--- Assignments/P05/helpers.hpp
+++ Assignments/P05/helpers.hpp
@@ -1,21 +1,53 @@
+/*****************************************************************************
+*
+*  Author:           Jarette Greene
+*  Email:            jarettegreene09@gmail.com
+*  Label:            P05
+*  Title:            BolNiverse Fight Club
+*  Course:           CMPS 2143
+*  Semester:         Fall 2022
+*
+*  Description:
+*		This Hpp file contains function that allows for a string to be 
+*       vector and get split based on a delimiter. The vector can be passed
+*       into a function/method so that the seperate information in the string 
+*       can be used. Example the tokenize function creates a string like -> [2.d.4] 
+*       and that string can be passed into the dice class methods to create 
+*       a set of die.
+*
+*  Usage:
+*        N/A
+*
+*  Files:
+*           helper.hpp          :contains class to tokenize a string
+*
+*****************************************************************************/
+// neccesary Libraries
 #include <iostream>
 #include <vector>
 #include <random>
 #include <string>
 
+//using namespace std
 using namespace std;
 
 #pragma once
-
 /**
- * @brief Splits a string into parts based on a delimiter
+ * Public : tokenize
+ *
+ * Description:
+ *        
+ *     Splits a string into parts based on a delimiter
  *
  *      For example the string: "2.d.12" would be turned into 
  *      a vector => ["2","d","12"] and returned
  * 
- * @param s 
- * @param delimiter 
- * @return vector<string> 
+ * Params:
+ *      string      s
+ *      string      delimiter
+ *
+ * Returns:
+ *      vector<string>      the tokenize string 
  */
 vector<string> tokenize(string s,string delimiter){
     size_t pos = 0;
@@ -27,16 +59,22 @@ vector<string> tokenize(string s,string delimiter){
     tokens.push_back(s);
     return tokens;
 }
-
 /**
- * @brief Chooses a random item from a vector of strings
+ * Public : randomChoice
+ *
+ * Description:
+ *        
+ *     Chooses a random item from a vector of strings
  * 
  *      Example: 
  *          given => ["1.d.4","1.d.6","1.d.8"]
  *          Choose one of the 3 choices at random and return it.
  * 
- * @param choices 
- * @return string 
+ * Params:
+ *      vector<string>      choices
+ *
+ * Returns:
+ *      string      the choice from the possible selection 
  */
 string randomChoice(vector<string> choices){
     return choices[rand() % choices.size()];
