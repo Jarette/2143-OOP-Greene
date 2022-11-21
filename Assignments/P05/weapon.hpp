bb3c17a673c3cf61776ee7ded2be7bd981dbdefd
Jarette Greene <jarettegreene09@gmail.com>
2022-11-21 09:52:34 -0600

    weapon comments

--- Assignments/P05/weapon.hpp
+++ Assignments/P05/weapon.hpp
@@ -1,3 +1,27 @@
+/*****************************************************************************
+*
+*  Author:           Jarette Greene
+*  Email:            jarettegreene09@gmail.com
+*  Label:            P05
+*  Title:            BolNiverse Fight Club
+*  Course:           CMPS 2143
+*  Semester:         Fall 2022
+*
+*  Description:
+*		    This Hpp file contains class used to create the weapon that the 
+*       fighter will be used. Contianing the name and damage that the 
+*       weapon will do based on the roll of a specified set of dice
+*
+*  Usage:
+*        N/A
+*
+*  Files:
+*           helper.hpp          :contains functions to tokenize a string
+*           dice.hpp            :contains classes to create dices
+*           weapon.hpp          :contains the class to create the weapon used by the fighters
+*
+*****************************************************************************/
+//necessary Libraries
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,10 +29,47 @@
 #include "dice.hpp"
 #include "helpers.hpp"
 
+//using namespace standard
 using namespace std;
 
 #pragma once
-
+/**
+ * Weapon
+ * 
+ * Description: 
+ *      This class is used to represent the weapons that the fighters
+ *      will be using in the main driver. This class gives the name of
+ *      and the damage the dice may be able to do based on a dice roll
+ * 
+ * Public Methods
+ *                            Weapon()
+ *                            Weapon(vector<string> choices)                
+ *                            Weapon(string n, string d)
+ *      double                use()
+ *      string                getName()
+ *      string                getDamage()
+ *      friend ostream&       operator<<(ostream& os, const Weapon& w)
+ * 
+ * Private Methods:   
+ *      string                   name
+ *      string                   damage
+ *      Dice*                    die
+ * Usage:
+ *      
+ *      Weapon W1                     //creates a default weapon of Fist&Feet 
+ *                                    // default damage
+ *      Weapom W2("1.d.10","2.d.4")   //creates weapon that damage is random of 
+ *                                    //the choices passed
+ *      Weapon W3("Sword","1.d.10")   //creates a weapon with the parameters passed
+ *      W3.use()                      // rolls the dice to return the damage dealt
+ *                                    // by weapons
+ *      W3.getName()                  // returns the name of the weapon
+ *      W3.getDamage()                // returns the dice used for this weapon
+ *      cout << W3                    // using ostream to output the name and damage
+ *                                    // of the weapon
+ *        
+ *    
+*/ 
 class Weapon {
     string name;    // name of weapon
     string damage;  // damage per roll or "use"
